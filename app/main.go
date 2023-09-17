5083bf9f9ed483e55406456a6e49109bcd53d6f3
Jon Fingas <jon.fingas@gmail.com>
2023-09-17 11:55:54 -0500

    Test

--- app/main.go
+++ app/main.go
@@ -1,50 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
 	// Define the command you want to run and its arguments
-	cmd := exec.Command("cd /opt/minecraft-servers/VaultHunters && tmux new", "-s vaulthunters ./run.sh")
-
-	// Set up pipes for the command's standard input, output, and error
-	cmd.Stdin = nil
-	cmd.Stderr = nil
-
-	// Create a pipe to capture the command's output
-	stdout, err := cmd.StdoutPipe()
+	err := serve()
 	if err != nil {
-		fmt.Println("Error creating StdoutPipe:", err)
-		return
+		panic(err)
 	}
 
-	// Start the command
-	err = cmd.Start()
+}
+
+func serve() error {
+	log, err := os.Create("output.log")
 	if err != nil {
-		fmt.Println("Error starting command:", err)
-		return
+		return err
 	}
-
-	// Read the output
-	output := make([]byte, 4096)
-	n, err := stdout.Read(output)
-	if err != nil {
-		fmt.Println("Error reading from stdout:", err)
-		return
-	}
-
-	// Convert the output to a string and print it
-	fmt.Println("Output:")
-	fmt.Println(string(output[:n]))
-
-	// Wait for the command to finish
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Println("Error waiting for command:", err)
-		return
-	}
-
-	fmt.Println("Command finished successfully.")
+	defer log.Close()
+	cmd := exec.Command("bash", " tmux new -s vaulthunters sh /opt/minecraft-servers/VaultHunters/run.sh")
+	cmd.Stdout = log
+	cmd.Stderr = log
+	return cmd.Run()
 }
